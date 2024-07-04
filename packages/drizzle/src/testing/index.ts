4ba0c5362a2493066846d32a15f3a5aa7aa18594
Justin Spears <speajus@gmail.com>
2024-07-03 18:22:29 -0700

    update

--- packages/drizzle/src/testing/index.ts
+++
@@ -1,9 +0,0 @@
-import {
-  createTestLibrary,
-  findTestPackageRoot,
-  TypeSpecTestLibrary,
-} from "@typespec/compiler/testing";
-export const DrizzleTestLibrary: TypeSpecTestLibrary = createTestLibrary({
-  name: "drizzle",
-  packageRoot: await findTestPackageRoot(import.meta.url),
-});
